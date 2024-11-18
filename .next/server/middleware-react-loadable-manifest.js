46e2b1120e8a0a3427943f2eeffe091bd7989480
shervinkayvon <shervinkayvon@gmail.com>
2024-11-18 15:47:28 -0800

    add lazy load

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> lodash\":{\"id\":\"app/page.tsx -> lodash\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_lodash_lodash_js.js\"]}}"
